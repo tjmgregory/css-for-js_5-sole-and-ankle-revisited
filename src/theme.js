ac6b344bf483d0e6a2b8c2c55cce54b5890f9f33
Theo Gregory <tjmg@outlook.com>
2022-12-05 08:07:57 +0000

    chore: rmeove console warning

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,9 @@
-export default {
+const theme = {
   breakpoints: {
     laptopAndDown: `(max-width: ${1300 / 16}rem)`,
     tabletAndDown: `(max-width: ${950 / 16}rem)`,
     phoneAndDown: `(max-width: ${600 / 16}rem)`
   }
-}
+};
+
+export default theme;
