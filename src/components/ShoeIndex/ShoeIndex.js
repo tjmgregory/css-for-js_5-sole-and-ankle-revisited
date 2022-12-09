3963f22985d0b8575a22ab36ce2bc42bd781a213
Theo Gregory <tjmg@outlook.com>
2022-12-09 09:52:07 +0000

    feat: ex6 - replace weights

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { WEIGHTS } from '../../constants';
-
 import Breadcrumbs from '../Breadcrumbs';
 import Select from '../Select';
 import Spacer from '../Spacer';
@@ -88,7 +86,7 @@ const Header = styled.header`
 
 const Title = styled.h2`
   font-size: 1.5rem;
-  font-weight: ${WEIGHTS.medium};
+  font-weight: ${({theme}) => theme.font.weight.medium};
 `;
 
 const SortFilterWrapper = styled.div`
